2775c05d8f89392f9020d0d92e2758b96ddd01fb
yusheng-ngo-2014 <yusheng.ngo.2014@smu.edu.sg>
2018-10-17 20:56:40 +0800

    Initial commit Project Set-up --- Fixing deploy script

--- do_deploy.sh
+++ do_deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 zip -q -r apps.zip server
-yes | scp -v -r ./apps.zip $DROPLET_USER@$DROPLET_IP:/home/apps
-yes | ssh -v $DROPLET_USER@$DROPLET_IP "unzip -o apps.zip"
-yes | ssh -v $DROPLET_USER@$DROPLET_IP "pm2 stop ./server/server.js"
-yes | ssh -v $DROPLET_USER@$DROPLET_IP "pm2 start ./server/server.js"
+scp -v -r ./apps.zip $DROPLET_USER@$DROPLET_IP:/home/apps
+ssh -v $DROPLET_USER@$DROPLET_IP "unzip -o apps.zip"
+ssh -v $DROPLET_USER@$DROPLET_IP "rm apps.zip"
+ssh -v $DROPLET_USER@$DROPLET_IP "pm2 stop ./server/server.js"
+ssh -v $DROPLET_USER@$DROPLET_IP "pm2 start ./server/server.js"
