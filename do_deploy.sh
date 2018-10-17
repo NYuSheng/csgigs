bdc14aafeb656b3500e26047d29f6b651d85a38a
paul <paul.sp.ng@gmail.com>
2018-10-17 17:14:31 +0800

    Fix deployment script

--- do_deploy.sh
+++ do_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 zip -r apps.zip server
-scp -v -r ./apps.zip $DROPLET_USER@$DROPLET_IP:/home/apps
-ssh -v $DROPLET_USER@$DROPLET_IP "unzip apps.zip"
-ssh -v $DROPLET_USER@$DROPLET_IP "node ./server/server.js"
+scp -v -r ./apps.zip APPS@$DROPLET_IP:/home/apps
+ssh -v APPS@$DROPLET_IP "unzip -o apps.zip"
+ssh -v APPS@$DROPLET_IP "pm2 stop ./server/server.js"
+ssh -v APPS@$DROPLET_IP "pm2 start ./server/server.js"
