1b1d2414910a401cbdbae607d73e5167bb1ae746
Ernest Che <ernestche.2014@sis.smu.edu.sg>
2018-11-16 01:38:32 +0900

    [BACKEND] Added temp method and route for aggregate query for creation of gigs

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -20,14 +20,107 @@ exports.gig_create = asyncMiddleware(async (req, res, next) => {
     );
 
     return gig.save().then(gigCreated => {
-        res.status(200).send({
-            "gig" : gigCreated
+
+        return Gig
+            .aggregate([
+            {$match: {name: req.body.name}},
+            {
+                $lookup: {
+                    from: 'tasks',
+                    localField: 'name',
+                    foreignField: 'gig_name',
+                    as: 'task_list'
+                }
+            }
+        ])
+            .populate('user_participants')
+            .exec().then((gigs_retrieved) => {
+                if(gigs_retrieved.length === 0){
+                    return res.status(400).send({
+                        error: 'Cannot find any GIGs: ' + req.body.name
+                    });
+                }
+                res.status(200).send({
+                    gigs: gigs_retrieved
+                });
+            }).catch(err=>{
+            res.status(400).send({error: err});
         });
     }).catch(err => {
         console.log(err);
         res.status(400).send({error : err});
     });
 });
+
+exports.gig_create_temp = asyncMiddleware(async (req, res, next) => {
+    let gig = new Gig(
+        {
+            name :req.body.name,
+            points_budget : req.body.points_budget,
+            status : "NOT STARTED",
+            user_admins : req.body.user_admins,
+
+            //Possible required fields in creation
+            user_participants: ["erny_che"],
+            user_attendees: []
+        }
+    );
+
+    return gig.save().then(gigCreated => {
+        console.log(aggregation_with_tasks_and_users(req.body.name));
+        return Gig
+            .aggregate(aggregation_with_tasks_and_users(req.body.name))
+            .exec().then((gigs_retrieved) => {
+                if(gigs_retrieved.length === 0){
+                    return res.status(400).send({
+                        error: 'Cannot find any GIGs: ' + req.body.name
+                    });
+                }
+                res.status(200).send({
+                    gigs: gigs_retrieved
+                });
+            }).catch(err=>{
+            res.status(400).send({error: err});
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send({error : err});
+    });
+});
+
+function aggregation_with_tasks_and_users(gig_name){
+    return [
+        {'$match': {'name': gig_name}},
+        {'$lookup': {
+            'from': 'tasks',
+            'localField': 'name',
+            'foreignField': 'gig_name',
+            'as': 'task_list'
+        }},
+        { "$unwind": "$user_participants" },
+        { "$lookup": {
+            "from": "users",
+            "localField": "user_participants",
+            "foreignField": "user_name",
+            "as": "userObjects"
+        }},
+        { "$unwind": "$userObjects" },
+        { "$group": {
+        "_id": "$_id",
+        "rc_channel_id": {"$first": "$rc_channel_id"},  
+        "user_admins": {"$first": "$user_admins"},
+        "user_participants": { "$push": "$user_participants" },
+        "userObjects": { "$push": "$userObjects" },
+        "user_attendees": {"$first": "$user_attendees"},
+        "name": {"$first": "$name"},
+        "points_budget": {"$first": "$points_budget"},
+        "status": {"$first": "$status"},
+        "createdAt": {"$first": "$createdAt"},
+        "__v": {"$first": "$__v"},
+        "task_list": {"$first": "$task_list"}
+        }}
+    ]
+}
 
 exports.gigs_details = asyncMiddleware(async (req, res, next) => {
 
@@ -42,7 +135,9 @@ exports.gigs_details = asyncMiddleware(async (req, res, next) => {
 });
 
 exports.gig_details = asyncMiddleware(async (req, res, next) => {
-    return Gig.findOne({name: req.params.name}).exec().then((gig_retrieved) => {
+    return Gig.findOne({name: req.params.name})
+        .populate('user_participants')
+        .exec().then((gig_retrieved) => {
         if(gig_retrieved === null){
             return res.status(400).send({
                 error: 'Cannot find gig of name ' + req.params.name
