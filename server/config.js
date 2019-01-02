99140c2d6f234ef68069e9460e3a088e8e2dbea7
Ernest Che <ernestche.2014@sis.smu.edu.sg>
2019-01-02 14:46:37 +0900

    [BACKEND] Relocate integration tests in gig unit test, setup config for mongodb test environment

--- server/config.js
+++ server/config.js
@@ -1,26 +1,34 @@
 const env = process.env.NODE_ENV; // 'development' or 'production'
 const development = {
-    db: {
-        // host: 'test1:test123@ds031895.mlab.com',
-        // port: 31895,
-        // name: 'projectgigstest'
-        host: 'localhost',
-        port: 27017,
-        name: 'test'
-    }
+  db: {
+    // host: 'test1:test123@ds031895.mlab.com',
+    // port: 31895,
+    // name: 'projectgigstest'
+    host: "localhost",
+    port: 27017,
+    name: "test"
+  }
+};
+
+const test = {
+  db: {
+    host: "localhost",
+    port: 27017,
+    name: "csgigs-test"
+  }
 };
 
 const production = {
-    db: {
-        host: 'localhost',
-        port: 27017,
-        name: 'csgigs-admin'
-    }
+  db: {
+    host: "localhost",
+    port: 27017,
+    name: "csgigs-admin"
+  }
 };
 
 const config = {
-    development,
-    production
+  development,
+  production
 };
 
 module.exports = config[env];
