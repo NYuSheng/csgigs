e977d625b2189fa54525c93132f84d8f3835691e
brandontjq <jinquan.tan.2014@sis.smu.edu.sg>
2019-01-03 09:05:46 +0800

    merging

--- server/config.js
+++ server/config.js
@@ -1,26 +1,35 @@
-const env = process.env.NODE_ENV; // 'development' or 'production'
+const env = process.env.NODE_ENV; // 'development' or 'production' or 'test'
 const development = {
-    db: {
-        //host: 'test1:test123@ds031895.mlab.com',
-        //port: 31895,
-        //name: 'projectgigstest'
-        host: 'localhost',
-        port: 27017,
-        name: 'test'
-    }
+  db: {
+    // host: "test1:test123@ds031895.mlab.com",
+    // port: 31895,
+    // name: "projectgigstest"
+    host: "localhost",
+    port: 27017,
+    name: "test"
+  }
+};
+
+const test = {
+  db: {
+    host: "localhost",
+    port: 27017,
+    name: "csgigs-test"
+  }
 };
 
 const production = {
-    db: {
-        host: 'localhost',
-        port: 27017,
-        name: 'csgigs-admin'
-    }
+  db: {
+    host: "localhost",
+    port: 27017,
+    name: "csgigs-admin"
+  }
 };
 
 const config = {
-    development,
-    production
+  development,
+  production,
+  test
 };
 
 module.exports = config[env];
