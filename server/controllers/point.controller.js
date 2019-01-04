ec9fcd0d0b8ecfe876f899365a1d63ebf2fd47dc
NYuSheng <44161554+NYuSheng@users.noreply.github.com>
2019-01-04 14:30:18 +0800

    [UI] Brownie points allocation --- Refactoring point controller async await

--- server/controllers/point.controller.js
+++ server/controllers/point.controller.js
@@ -1,42 +1,42 @@
 const Point = require("../models/point.model");
 const ObjectID = require("mongodb").ObjectID;
 const LogConfig = require("../log-config");
+const asyncMiddleware = require("../utils/asyncMiddleware");
 
-exports.assign_points = function(req, res) {
+exports.assign_points = asyncMiddleware(async function(req, res) {
   const points_assigned = new Point({
     user_id: req.params.user_id,
     gig_id: ObjectID(req.body.gig_id),
     points: req.body.points
   });
-
-  return points_assigned
-    .save()
-    .then(result => {
-      res.status(200).send({
-        points_record: result
-      });
-    })
-    .catch(err => {
-      LogConfig.error(JSON.stringify(err));
-      res.status(500).send({ error: err });
+  try {
+    const assigned_points = await points_assigned.save();
+    if (!assigned_points) {
+      throw `Unable to assign points to ${req.params.user_id}`;
+    }
+    res.status(200).send({
+      points_record: assigned_points
     });
-};
+  } catch (error) {
+    LogConfig.error(JSON.stringify(error));
+    res.status(500).send({ error: error });
+  }
+});
 
-exports.get_points_gig = function(req, res) {
-  return Point.find({ gig_id: ObjectID(req.params.gig_id) })
-    .then(result => {
-      if (!result) {
-        return res.status(404).send({
-          error:
-            "Cannot find any points allocation for gig: " + req.params.gig_id
-        });
-      }
-      res.status(200).send({
-        gig_points_record: result
-      });
-    })
-    .catch(err => {
-      LogConfig.error(JSON.stringify(err));
-      res.status(500).send({ error: err });
+exports.get_points_gig = asyncMiddleware(async function(req, res) {
+  const matchCriteria = {
+    gig_id: ObjectID(req.params.gig_id)
+  };
+  try {
+    const points_gig = await Point.find(matchCriteria);
+    if (!points_gig) {
+      throw `Unable to find point allocations for gig: ${req.params.gig_id}`;
+    }
+    res.status(200).send({
+      gig_points_record: points_gig
     });
-};
+  } catch (error) {
+    LogConfig.error(JSON.stringify(error));
+    res.status(500).send({ error: error });
+  }
+});
