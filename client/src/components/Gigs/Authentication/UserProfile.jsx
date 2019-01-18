bcb66bcfb82ef7e6242948ee57e7876417ac396c
Rowan <rowanwork@users.noreply.github.com>
2019-01-18 14:02:10 +0800

    Fix linting, bump CI

--- client/src/components/Gigs/Authentication/UserProfile.jsx
+++ client/src/components/Gigs/Authentication/UserProfile.jsx
@@ -1,52 +1,52 @@
-var UserProfile = (function () {
+const UserProfile = (function() {
+  const login = function(user) {
+    fetch(
+      `https://csgigs.com/api/v1/users.getAvatar?username=${user.me.username}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      }
+    ).then(output => {
+      if (output.status === 200) {
+        user.me.avatar = output.url;
+      } else {
+        // eslint-disable-next-line no-console
+        console.log("Error - User avatar not found");
+      }
+      sessionStorage.setItem("user", JSON.stringify(user));
+    });
+  };
 
-    var login = function (user) {
-        fetch(`https://csgigs.com/api/v1/users.getAvatar?username=${user.me.username}`, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'}
-        }).then(output => {
-            if (output.status === 200) {
-                user.me.avatar = output.url;
-            } else {
-                console.log("Error - User avatar not found");
-            }
-            sessionStorage.setItem('user', JSON.stringify(user))
-        })
+  const authenticate = function() {
+    try {
+      const user = JSON.parse(sessionStorage.getItem("user"));
+      if (user) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (exception) {
+      return false;
     }
+  };
 
-    var authenticate = function () {
-        try {
-            const user = JSON.parse(sessionStorage.getItem('user'));
-            if (user) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (exception) {
-            return false;
-        }
-    }
+  const getAuthSet = function() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    const authSet = { token: user.authToken, userId: user.userId };
+    return authSet;
+  };
 
-    var getAuthSet = function () {
-        const user = JSON.parse(sessionStorage.getItem('user'));
-        var authSet = {};
-        authSet["token"] = user.authToken;
-        authSet["userId"] = user.userId;
-        return authSet;
-    }
+  const getUser = function() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    return user;
+  };
 
-    var getUser = function () {
-        const user = JSON.parse(sessionStorage.getItem('user'));
-        return user;
-    }
-
-    return {
-        login: login,
-        authenticate: authenticate,
-        getAuthSet: getAuthSet,
-        getUser: getUser
-    }
-
+  return {
+    login: login,
+    authenticate: authenticate,
+    getAuthSet: getAuthSet,
+    getUser: getUser
+  };
 })();
 
 export default UserProfile;
